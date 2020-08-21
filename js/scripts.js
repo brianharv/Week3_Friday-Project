52f03ff61ba58cbbf271bd747b7c014ace52d2b5
Brian Harvey <brian.harv3y@gmail.com>
2020-08-21 13:40:32 -0700

    fshow function looping but not generating correct output

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 //Business Logic
-
 function roboTalk(number) {
-  let roboArray = []; //Need number to equal length of array
+  let roboArray = [];
   for (let index = 0; index <= number; index++) {
     //roboArray.push(index);
+    alert("working");
     let digit = roboArray[index];
     if (digit === 1) {
       roboArray.replace("Beep!");
+    } else if (digit === 2) {
+      roboArray.replace("Boop!");
+    } else if (digit === 3) {
+      roboArray.replace("Won't you be my neighbor?");
     }
-    )
-  }
-}
+    alert("stillworking");
+  } 
+  return roboArray.push();
+} 
 
 
 //User Interface Logic
@@ -19,7 +24,6 @@ $(document).ready(function() {
     event.preventDefault();
     const userInput = parseInt($("input#userInput").val());
 
-    $(".form-output").text(userInput);
-   // $(".form-output").text(roboTalk(userInput));
+    $(".form-output").text(roboTalk(userInput));
   });
 });
