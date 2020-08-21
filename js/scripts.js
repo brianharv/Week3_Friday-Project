8f0477319b40053ee2ffa6d1fcb15eeefb3f4ea6
Brian Harvey <brian.harv3y@gmail.com>
2020-08-21 11:05:48 -0700

    further troubleshoot js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 //Business Logic
 
+function roboTalk(number) {
+  let roboArray = [];
+  for (let index = 0; index < number; index +=1) {
+    console.log("working");
+    roboArray.push();
+    
+  }
+}
 
 
 //User Interface Logic
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const userInput = parseInt($("input#userInput").val());
-    $(".form-output").text(userInput);
+    const number = parseInt($("input#userInput").val());
+    $(".form-output").text(number);
   });
 });
