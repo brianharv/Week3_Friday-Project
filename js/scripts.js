25eaefc25ab398ffa12fcedc003119c93cd8c0f1
Brian Harvey <brian.harv3y@gmail.com>
2020-08-21 10:42:14 -0700

    fix form  typos

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
 //Business Logic
-let userArray = [];
-let userInput = userArray.length -1
-let roboArry = [];
-for (let index = 0; index < userInput; i++) {
-  roboArray.push();
-  console.log(userInput);
-}
+
+
 
 //User Interface Logic
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     const userInput = parseInt($("input#userInput").val());
+    $(".form-output").text(userInput);
   });
 });
